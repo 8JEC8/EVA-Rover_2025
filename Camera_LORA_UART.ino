bd52ae56f836389e70fb86f22a00cd6fee890b4c
8JEC8 <ecastellanos220@gmail.com>
2025-09-25 11:55:53 -0600

    Working Bidirectional LoRa + Bidirectional Serial (CAM)

--- Camera_LORA_UART.ino
+++ Camera_LORA_UART.ino
@@ -5,14 +5,14 @@
 #define LORA_FREQ 433E6
 
 // LoRa pins for ESP32-CAM
-#define LORA_SCK   14   // SCK
-#define LORA_MISO  12   // MISO
-#define LORA_MOSI  13   // MOSI
-#define LORA_SS    15   // CS (NSS)
-#define LORA_DIO0   4   // DIO0
+#define LORA_SCK   14
+#define LORA_MISO  12
+#define LORA_MOSI  13
+#define LORA_SS    15
+#define LORA_DIO0   4
 
 // WS2812 LED strip
-#define LED_PIN    16     // choose a safe GPIO (2 or 16 recommended)
+#define LED_PIN    16
 #define NUM_LEDS   8
 Adafruit_NeoPixel strip(NUM_LEDS, LED_PIN, NEO_GRB + NEO_KHZ800);
 
@@ -27,10 +27,7 @@ void setup() {
   Serial.begin(115200);
   while (!Serial);
 
-  // Initialize SPI with custom pins for ESP32-CAM
   SPI.begin(LORA_SCK, LORA_MISO, LORA_MOSI, LORA_SS);
-
-  // Attach LoRa with defined CS and DIO0 pins
   LoRa.setPins(LORA_SS, -1, LORA_DIO0);
 
   if (!LoRa.begin(LORA_FREQ)) {
@@ -38,51 +35,55 @@ void setup() {
     while (true);
   }
 
-  // LoRa
-  LoRa.setTxPower(20);             
-  LoRa.setSpreadingFactor(12);     
-  LoRa.setSignalBandwidth(125E3);  
-  LoRa.setCodingRate4(8);          
+  LoRa.setTxPower(20);
+  LoRa.setSpreadingFactor(12);
+  LoRa.setSignalBandwidth(125E3);
+  LoRa.setCodingRate4(8);
 
-  // Iniciar LEDs
   strip.begin();
-  strip.show(); // Apagar LEDs
+  strip.show(); // turn off LEDs
 }
 
 void loop() {
-  // 1. Check RECV
+  // --- 1. Receive LoRa messages ---
   int packetSize = LoRa.parsePacket();
   if (packetSize) {
     String received = "";
     while (LoRa.available()) {
       received += (char)LoRa.read();
     }
-    
-    String ack = "ACK_" + received;
+    received.trim(); // remove whitespace/newlines
+
+    // Send acknowledgment back over LoRa
     LoRa.beginPacket();
-    LoRa.print(ack);
+    LoRa.print("ACK_" + received);
     LoRa.endPacket();
 
-    if (received == ".RED") {
-      setColor(10, 0, 0);
-    } else if (received == ".GREEN") {
-      setColor(0, 10, 0);
-    } else if (received == ".BLUE") {
-      setColor(0, 0, 10);
-    } else if (received == ".YELLOW") {
-      setColor(10, 10, 0);
-    } else if (received == ".CYAN") {
-      setColor(0, 10, 10);
-    } else if (received == ".MAGENTA") {
-      setColor(10, 0, 10);
-    } else if (received == ".WHITE") {
-      setColor(10, 10, 10);
-    } else if (received == ".ORANGE") {
-      setColor(10, 4, 0);
-    } else if (received == ".PURPLE") {
-      setColor(5, 0, 10);
-    } else if (received == ".OFF") {
-      setColor(0, 0, 0);
+    // --- Check if it's a color command ---
+    if (received == ".RED") setColor(10,0,0);
+    else if (received == ".GREEN") setColor(0,10,0);
+    else if (received == ".BLUE") setColor(0,0,10);
+    else if (received == ".YELLOW") setColor(10,10,0);
+    else if (received == ".CYAN") setColor(0,10,10);
+    else if (received == ".MAGENTA") setColor(10,0,10);
+    else if (received == ".WHITE") setColor(10,10,10);
+    else if (received == ".ORANGE") setColor(10,4,0);
+    else if (received == ".PURPLE") setColor(5,0,10);
+    else if (received == ".OFF") setColor(0,0,0);
+    // --- Otherwise, forward to STA via Serial ---
+    else {
+      Serial.println(received);
+    }
+  }
+
+  // --- 2. Check Serial from STA and forward over LoRa ---
+  while (Serial.available()) {
+    String staResponse = Serial.readStringUntil('\n');
+    staResponse.trim(); // clean extra whitespace
+    if (staResponse.length() > 0) {
+      LoRa.beginPacket();
+      LoRa.print(staResponse);
+      LoRa.endPacket();
     }
   }
 }
